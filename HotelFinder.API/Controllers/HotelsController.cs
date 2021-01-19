bfae696c0cf8e27995eb5e57f97f2f1c695c5790
Mete Çapar <mete.capar@ext.kocdigital.com>
2021-01-19 15:36:16 +0300

    Swagger

--- HotelFinder.API/Controllers/HotelsController.cs
+++ HotelFinder.API/Controllers/HotelsController.cs
@@ -19,28 +19,53 @@ namespace HotelFinder.API.Controllers
         {
             _hotelService = hotelService;
         }
+
+
+        /// <summary>
+        /// Get All Hotels
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public List<Hotel> Get()
         {
             return _hotelService.GetAllHotels();
 
         }
+        /// <summary>
+        /// Get Hotel Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpGet("{id}")]
         public Hotel Get(int id)
         {
             return _hotelService.GetHotelById(id);
 
         }
+        /// <summary>
+        /// Create Hotel
+        /// </summary>
+        /// <param name="hotel"></param>
+        /// <returns></returns>
         [HttpPost]
         public Hotel Post([FromBody]Hotel hotel)
         {
             return _hotelService.CreateHotel(hotel);
         }
+        /// <summary>
+        /// Update a Hotel
+        /// </summary>
+        /// <param name="hotel"></param>
+        /// <returns></returns>
         [HttpPut]
         public Hotel Put([FromBody] Hotel hotel)
         {
             return _hotelService.UpdateHotel(hotel);
         }
+        /// <summary>
+        /// Delete a Hotel
+        /// </summary>
+        /// <param name="id"></param>
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
--- HotelFinder.API/Startup.cs
+++ HotelFinder.API/Startup.cs
@@ -23,6 +23,20 @@ namespace HotelFinder.API
             services.AddControllers();
             services.AddSingleton<IHotelService, HotelManager>();
             services.AddSingleton<IHotelRepository, HotelRepository>();
+            services.AddSwaggerDocument(config =>
+            {
+                config.PostProcess = (doc =>
+                  {
+                      doc.Info.Title = "All Hotels Api";
+                      doc.Info.Version = "1.0.13";
+                      doc.Info.Contact = new NSwag.OpenApiContact()
+                      {
+                          Name = "Mete CAPAR",
+                          Url = "http://capar.net",
+                          Email = "mete@capar.net"
+                      };
+                  });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,7 +48,8 @@ namespace HotelFinder.API
             }
 
             app.UseRouting();
-
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
