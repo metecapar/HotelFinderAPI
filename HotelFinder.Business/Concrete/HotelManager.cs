9b7e0d90a95289e672ba9c384fa58ed5b3710363
Mete Çapar <mete.capar@ext.kocdigital.com>
2021-01-19 13:18:50 +0300

    DotNet Core ile ilk API denemem

---
+++ HotelFinder.Business/Concrete/HotelManager.cs
@@ -0,0 +1,47 @@
+﻿using HotelFinder.Business.Abstract;
+using HotelFinder.DataAccess.Abstract;
+using HotelFinder.DataAccess.Contcrete;
+using HotelFinder.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelFinder.Business.Concrete
+{
+    public class HotelManager : IHotelService
+    {
+        private IHotelRepository _hotelRepository;
+        public HotelManager(IHotelRepository hotelRepository)
+        {
+            _hotelRepository =hotelRepository;
+        }
+        public Hotel CreateHotel(Hotel hotel)
+        {
+            return _hotelRepository.CreateHotel(hotel);
+        }
+
+        public void DeleteHotel(int id)
+        {
+            _hotelRepository.DeleteHotel(id);
+        }
+
+        public List<Hotel> GetAllHotels()
+        {
+            return _hotelRepository.GetAllHotels();
+        }
+
+        public Hotel GetHotelById(int id)
+        {
+            if (id > 0)
+            {
+                throw new Exception("Id 1 den kucuk olamaz");
+            }
+            return _hotelRepository.GetHotelById(id);
+        }
+
+        public Hotel UpdateHotel(Hotel hotel)
+        {
+            return _hotelRepository.UpdateHotel(hotel);
+        }
+    }
+}
