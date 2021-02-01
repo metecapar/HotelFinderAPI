bac3c43f0d181519e1926312f3cdc74dc2fc4bb9
Mete Çapar <mete.capar@ext.kocdigital.com>
2021-02-01 11:48:07 +0300

    SqlHealthChecks

--- HotelFinder.API/Startup.cs
+++ HotelFinder.API/Startup.cs
@@ -1,21 +1,32 @@
+using HealthChecks.UI.Client;
+using HealthChecks.UI.Configuration;
 using HotelFinder.Business.Abstract;
 using HotelFinder.Business.Concrete;
 using HotelFinder.DataAccess.Abstract;
 using HotelFinder.DataAccess.Contcrete;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace HotelFinder.API
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -37,8 +48,13 @@ namespace HotelFinder.API
                       };
                   });
             });
-        }
 
+            services.AddHealthChecks().AddSqlServer(Configuration.GetConnectionString("HotelDB"));
+
+           
+
+            services.AddHealthChecksUI().AddInMemoryStorage();
+        }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -48,11 +64,18 @@ namespace HotelFinder.API
             }
 
             app.UseRouting();
+           
             app.UseOpenApi();
             app.UseSwaggerUi3();
+            app.UseHealthChecks("/health", new HealthCheckOptions
+            {
+                Predicate = registration => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHealthChecksUI();
             });
         }
     }
